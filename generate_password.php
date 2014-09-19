85e1592d29ce851af120f150d8b4448c9809faf3
Caleb Davis <caleb.davis@gmail.com>
2014-09-19 12:04:16 -0700

    add bootstrap to generate_password.php

--- generate_password.php
+++ generate_password.php
@@ -3,8 +3,29 @@
   <head>
     <title>Generated xkcd-style Password</title>
     <meta charset='utf-8'/>
+
+    <!-- BEGIN 3rd Party Bootstrap inclusions -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <!--[if lt IE 9]>
+      <script -->
+        <!--src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js">
+      </script>
+      <script -->
+        <!--src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js">
+      </script>
+    <![endif]-->
+    <!-- END 3rd Party Bootstrap inclusions -->
+
   </head>
   <body>
+
+    <!-- BEGIN more Bootstrap inclusions -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- END more Boostratp inclusions -->
+
     <h1>Generated xkcd-style Password</h1>
     <?php 
       ini_set("auto_detect_line_endings", true);
