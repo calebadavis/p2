f0b024e9c4435c1c01212d1e9d737721660f768f
Caleb Davis <caleb.davis@gmail.com>
2014-09-19 21:54:31 -0700

    extract logic to separate module

---
+++ logic.php
@@ -0,0 +1,55 @@
+<?php
+      ini_set("auto_detect_line_endings", true);
+      $words = file("words.txt", FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+      $chars = file("special_chars.txt", FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+      $cnt = count($words); // How many words are in the dictionary?
+      $specialCharsCnt = count($chars); // How many available special chars?
+      $requestCnt = (int)$_POST['numWords']; // Password length (num of words)
+      $insertNum = (bool)$_POST['insertNum']; // Should password include a num?
+      $insertChar = (bool)$_POST['insertChar']; // Should it include a char?
+      $upperCase = (bool)$_POST['upperCase']; // Should one character be caps?
+      // Which word in the password should get an inserted number?
+      $wordToInsertNum = ($insertNum) ? rand(1, $requestCnt) : 0;
+
+      // Which word in the password should get an inserted special character?
+      $wordToInsertChar = ($insertChar) ? rand(1, $requestCnt) : 0;
+
+      // Which word in the password should get an upper case character?
+      $wordToUpperCase = ($upperCase) ? rand(1, $requestCnt) : 0;
+
+      $password = "";
+      echo("    <p>");
+
+      // The main loop: for each random word
+      for ($idx = 0; $idx < $requestCnt; ++$idx) {
+	$randIdx = rand(200, 6000);
+        $newWord = $words[$randIdx];
+
+        // If the user requested it, make one letter of one word in the 
+        // password upper-case.
+        if ($upperCase && ($wordToUpperCase == $idx + 1)) {
+          $capsIdx = rand(0, strlen($newWord)-1);
+	  $pfx = substr($newWord, 0, $capsIdx);
+          $charToChange = substr($newWord, $capsIdx, 1);
+          $sfx = substr($newWord, $capsIdx+1);
+          $newWord = $pfx . strtoupper($charToChange) . $sfx;
+	}
+
+        // If the user requested a random number to be inserted in one word...
+        if ($insertNum && ($wordToInsertNum == $idx + 1)) {
+          $numVal = rand(0, 9);
+          $insertPos = rand(0, strlen($newWord));
+          $newWord = substr_replace($newWord, $numVal, $insertPos, 0);
+	}
+
+        // If the user requested a randomly placed special character....
+        if ($insertChar && ($wordToInsertChar == $idx + 1)) {
+          $charVal = $chars[rand(0, count($chars) - 1)];
+          $insertPos = rand(0, strlen($newWord));
+          $newWord = substr_replace($newWord, $charVal, $insertPos, 0);
+	}
+
+        $password = $password . $newWord;
+        if ($idx+1 < $requestCnt) $password = $password . '-';
+      }
+?>
